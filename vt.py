70d053095c07651ab0450121484da00e628e06f6
Maksim <panasenkomaksim@gmail.com>
2021-04-26 22:13:28 +0300

    refactor

---
+++ vt.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.8
+
+import requests
+import hashlib
+
+
+class Hash(object):
+    def __init__(self):
+        self.m = hashlib.md5()
+
+    def hashing(self):
+        m.update("000005fab4534d05api_key9a0554259914a86fb9e7eb014e4e5d52permswrite")
+        print m.hexdigest()
+
+
+class File(object):
+
+    def open_file(self):
+        pass
+
+    
+
+if __name__ == "__main__":
+    main = Main()
