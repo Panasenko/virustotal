dc56279fc3e4415935f3fad31dae57c6d0e641d8
Maksim <panasenkomaksim@gmail.com>
2021-04-27 23:06:56 +0300

    freez

--- vt.py
+++ vt.py
@@ -2,23 +2,57 @@
 
 import requests
 import hashlib
+import optparse
+
+class Main(object):
+    def __init__(self):
+        self.option = Options()
+        self.hash = Hash()
+        self.start()
+
+    def start(self):
+        file_path = self.option.opt_parser()
+        hash_sum = self.hash.hashing(file_path)
+
+        print(hash_sum)
+
+
+class Options(object):
+    def opt_parser(self):
+        parser = optparse.OptionParser()
+        parser.add_option("-f", "--file",
+                          dest="file_path",
+                          help="Add file path for chack")
+
+        return self.check_input(parser)
+
+    def check_input(self, parser):
+        options = parser.parse_args()[0]
+
+        if not options.file_path:
+            mes = "[-] Please specify an interface, use -- help for more info."
+            parser.error(mes)
+
+        return options.file_path
 
 
 class Hash(object):
     def __init__(self):
         self.m = hashlib.md5()
 
-    def hashing(self):
-        m.update("000005fab4534d05api_key9a0554259914a86fb9e7eb014e4e5d52permswrite")
-        print m.hexdigest()
+    def hashing(self, file_path):
+        with open(file_path) as f:
+            self.m.update(f.read().encode("utf-8"))
+            return self.m.hexdigest()
 
 
-class File(object):
-
-    def open_file(self):
+class Call(object):
+    def __init__(self):
         pass
 
-    
+    def requests(self, url):
+        pass
+
 
 if __name__ == "__main__":
     main = Main()
